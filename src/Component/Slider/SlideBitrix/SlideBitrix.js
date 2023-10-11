8e9b3870b5e2e9b7ac6462f0de976312422db77a
sergey <sapokrovsky@gmail.com>
2023-10-11 14:21:08 +0300

    push on Gitlab

---
+++ src/Component/Slider/SlideBitrix/SlideBitrix.js
@@ -0,0 +1,16 @@
+import './SlideBitrix.css'
+
+
+function SlideBitrix() {
+    return (
+      <div className="slidebitrix">
+        <div className='slidebitrix__block'>
+            <p className='slidebitrix__title'>БИТРИКС 24</p>
+            <p className='slidebitrix__subtitle'>Horem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Horem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Horem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
+            <button className='slidebitrix__button-link'>Перейти</button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default SlideBitrix;
--- src/Component/Slider/Slider.js
+++ src/Component/Slider/Slider.js
@@ -1,9 +1,11 @@
 import SlideMain from "./SlideMain/SlideMain";
+import SlideBitrix from "./SlideBitrix/SlideBitrix";
 
 function Slider() {
     return (
       <div className="Slider">
         <SlideMain/>
+        {/* <SlideBitrix/> */}
       </div>
     );
   }
